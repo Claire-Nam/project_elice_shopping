38e1da0b999b6a038d4ab7ba8601f34efeacd2cd
Claire-Nam <nbr1023@naver.com>
2024-06-14 01:04:27 +0900

    css 2차 수정(주문완료 css 1차 종료)

--- order_Page/주문완료/orderComplite.js
+++ order_Page/주문완료/orderComplite.js
@@ -1,10 +1,12 @@
-const goOrderDetail = document.getElementsByClassName("orderDetail");
-const goHome = document.getElementsByClassName("goHome");
+document.addEventListener("DOMContentLoaded", function () {
+  const gotoHome = document.querySelector(".goHome");
+  const goOrderDetail = document.querySelector(".orderDetail");
 
-goOrderDetail.addEventListner("click", () => {
-  window.location.href = "#"; //해당 주문 내역 페이지의 URL로 이동
-});
+  goOrderDetail.addEventListener("click", () => {
+    window.location.href = "#"; // 해당 주문 내역 페이지의 URL로 이동
+  });
 
-goHome.addEventListner("click", () => {
-  window.location.href = "#"; //메인페이지 URL로 이동
+  gotoHome.addEventListener("click", () => {
+    window.location.href = "./perfume/index.html"; // 메인페이지 URL로 이동
+  });
 });
