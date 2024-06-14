6bebaa03227e08bc114ac0ea36f68dbbfa20fa4a
G-jjunny <rudwns9551@naver.com>
2024-06-14 17:43:43 +0900

    User branch생성

---
+++ public/myPage/myPage.js
@@ -0,0 +1,1 @@
+//        /mypage에서 User Date가 없으면 /signin 경로롤 리디렉
--- server.js
+++ server.js
@@ -8,19 +8,32 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  // ---- 이후 public 디렉토리 하위 각 폴더별로 라우터 변경
+  res.sendFile(path.join(__dirname, "public", "index.html")); // 브랜치 병합 이후 여러분들 폴더를 public폴더로 옮겨야함
 });
 
+// *********** User Page 관련 ***************
 app.get("/signup", (req, res) => {
   res.sendFile(path.join(__dirname, "public/sign", "signup.html"));
 });
-
 app.get("/signin", (req, res) => {
   res.sendFile(path.join(__dirname, "public/sign", "signin.html"));
 });
 app.get("/mypage", (req, res) => {
   res.sendFile(path.join(__dirname, "public/myPage", "myPage.html"));
 });
+
+// *********** Order Page 관련 ***************
+// app.get("/orderList", (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "public/order_Page", "파일명 넣어주세요.html")
+//   );
+// });
+// app.get("/orderList", (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "public/order_Page", "파일명 넣어주세요.html")
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
