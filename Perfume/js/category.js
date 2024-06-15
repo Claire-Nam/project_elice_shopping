50c68c21d6291e29a4a2b970eecc6cb631127ef9
J-yun-ji <qety_1356@naver.com>
2024-06-15 12:13:12 +0900

    clone

--- Perfume/js/category.js
+++
@@ -1,4 +0,0 @@
-const ourStory = document.getElementById("ourstory");
-const best = document.getElementById("best");
-const man = document.getElementById("men");
-const women = document.getElementById("women");
