ca140d85e72d4eb914dddc79da1fa4e82af42c72
Hyeonsu Lee <incleaf@gmail.com>
2024-06-19 20:46:55 +0900

    주문 삭제 연동

--- public/order_Page/order_list/order_list.js
+++ public/order_Page/order_list/order_list.js
@@ -1,12 +1,25 @@
 const cancelBtn = document.querySelectorAll(".cancel-btn");
 
-function onCancelButtonClick() {
+async function onCancelButtonClick(e) {
   const check = confirm("정말 주문을 취소하시겠습니까?");
-  console.log(check);
 
-  if (check == true) {
-    window.open("#", _name);
+  if (!check) {
+    return;
   }
+
+  const orderId = e.target.getAttribute("attr-order-id");
+
+  const result = await fetch(
+    `http://34.22.80.21/api/orders/orders?oid=${orderId}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  ).then((x) => x.json());
+
+  alert("삭제되었습니다.");
 }
 
 cancelBtn.forEach((el) => {
