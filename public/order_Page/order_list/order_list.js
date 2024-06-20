de09ac704ec3eaac118face07ee99cecb5b79ab0
Claire-Nam <nbr1023@naver.com>
2024-06-20 16:43:40 +0900

    js 모달 구축

--- public/order_Page/order_list/order_list.js
+++ public/order_Page/order_list/order_list.js
@@ -1,16 +1,36 @@
+document.addEventListener("DOMContentLoaded", loadOrderList());
 const cancelBtn = document.querySelectorAll(".cancel-btn");
 
 async function loadOrderList() {
-  const order_list = await fetch(
-    "http://localhost:5000/api/orders/66717406fbeacc5c007f7ddd",
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  ).then((res) => res.json());
+  try {
+    //서버에서 데이터 가져오기
+    const orderTable = document.getElementById("orderTable");
+    const response = await fetch(
+      "http://localhost:5000/api/orders/66717406fbeacc5c007f7ddd"
+    );
+    const orders = await response.json();
+
+    // 데이터를 테이블에 추가하기
+    orders.forEach((order) => {
+      let row = document.createElement("tr");
+      row.innerHTML = `
+      <td>${order.orderNumber}</td>
+      <td>${order.productImage}</td>
+      <td>${order.productName}</td>
+      <td>${order.price}</td>
+      <td>${order.quantity}</td>
+      `;
+      orderTable.querySelector("tbody").appendChild(row);
+    });
+  } catch (error) {
+    console.error("주문 데이터를 가져오는 중 오류가 발생했습니다.", error);
+  }
 }
+
+async () => {
+  const data = await loadOrderList();
+  const orderTable = document.getElementById("orderTable");
+};
 
 async function onCancelButtonClick(e) {
   const check = confirm("정말 주문을 취소하시겠습니까?");
@@ -25,7 +45,6 @@ async function onCancelButtonClick(e) {
           method: "DELETE",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
           },
         }
       );
