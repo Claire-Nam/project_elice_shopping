f3ccfadd5d4808b166b9f5402c507fe5a706a62d
J-yun-ji <qety_1356@naver.com>
2024-06-20 18:30:30 +0900

    JSON 생성

--- public/best/best.js
+++ public/best/best.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-function createPerfumeCard(name, price, img, description) {
-  return `
-  <div class="perfume">
-        <img src="../image/perfume1.jpg" class="perfumeImg" />
-        <img src="../image/shopping-cart.svg" class="svgCartBest" alt="cart" />
-        <p class="bestInfo" id="brandName">${name}</p>
-        <p class="bestInfo" id="perfumeName">옴므 코롱</p>
-        <p class="bestInfo" id="scent">${description}</p>
-        <p class="bestInfo" id="price">${price.toLocaleString()}원</p>
-      </div>
-  `;
-}
-
-document.addEventListener("DOMContentLoaded", async () => {
-  /**
-   * {
-    "_id": "60c72b3a9b1d8a4a3c4d2e42",
-    "id": "f47ac10b-58cc-4372-a567-0e02b2c3d491",
-    "name": "Perfume B",
-    "price": 65.5,
-    "description": "A luxurious fragrance with woody notes.",
-    "img": "perfume_b.jpg",
-    "sold": 15
-   */
-  const products = await fetch("http://34.22.80.21/api/products/products", {
-=======
 /*function createPerfumeCard(name, price, img, description) {
   return `
     <div class="perfume">
@@ -41,7 +14,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 document.addEventListener("DOMContentLoaded", async () => {
 
   const products = await fetch("http://34.22.80.21/api/products/", {
->>>>>>> a
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -53,8 +25,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     .join("");
 
   document.querySelector("#bestSection").innerHTML = html;
-<<<<<<< HEAD
-=======
 });*/
 function createPerfumeCard(
   name,
@@ -159,5 +129,4 @@ document.addEventListener("DOMContentLoaded", async () => {
       "#bestSection"
     ).innerHTML = `<p>Error loading data: ${error.message}</p>`;
   }
->>>>>>> a
 });
